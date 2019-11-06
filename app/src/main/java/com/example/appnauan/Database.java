c828c2948b099727c9b2d2c8b24f10bac03b13b2
HuyenTran112 <huyentranbui112@gmail.com>
2019-11-06 17:33:50 +0700

    update code dang ky user

--- app/src/main/java/com/example/appnauan/Database.java
+++ app/src/main/java/com/example/appnauan/Database.java
@@ -8,6 +8,8 @@ import android.database.sqlite.SQLiteStatement;
 
 import androidx.annotation.Nullable;
 
+import java.sql.Blob;
+
 public class Database extends SQLiteOpenHelper {
     public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -30,6 +32,21 @@ public class Database extends SQLiteOpenHelper {
         statement.bindString(2,nguyenlieu);
         statement.bindString(3,congthuc);
         statement.bindBlob(4,hinhanh);
+        //Thực thi insert
+        statement.executeInsert();
+    }
+    public void Insert_User(String tenhienthi, String mail, String matkhau, String loaiuser, byte[] hinhanh)
+    {
+        SQLiteDatabase database=getWritableDatabase();
+        String sql="INSERT INTO User VALUES(null,?,?,?,?,?)";
+        SQLiteStatement statement=database.compileStatement(sql);
+        statement.clearBindings();
+        //định dạng cho các ?
+        statement.bindString(1,tenhienthi);
+        statement.bindString(2,mail);
+        statement.bindString(3,matkhau);
+        statement.bindString(4,loaiuser);
+        statement.bindBlob(5,hinhanh);
         //Thực thi insert
         statement.executeInsert();
     }

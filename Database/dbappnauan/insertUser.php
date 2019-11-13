18a4ece859724bc9ed88aa16edf1ef78d9f28253
HuyenTran112 <huyentranbui112@gmail.com>
2019-11-13 22:40:45 +0700

    update code add user

---
+++ Database/dbappnauan/insertUser.php
@@ -0,0 +1,76 @@
+<?php
+require 'dbConnect.php';
+global $connection;
+$upload_path = 'imageuser/'; //this is our upload folder
+$server_ip = "10.80.255.137:8080"; //Getting the server ip
+$upload_url = 'http://'.$server_ip.'/dbappnauan/'.$upload_path;
+//response array
+$response = array();
+ 
+ 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+ 
+    //checking the required parameters from the request
+    if(isset($_POST['email']) and isset($_POST['tenhienthi']) and isset($_POST['matkhau']) and isset($_POST['maloainguoidung']))
+    {
+        $email=$_POST['email'];
+        $tenhienthi=$_POST['tenhienthi'];
+        $matkhau=$_POST['matkhau'];
+        $maloainguoidung=$_POST['maloainguoidung'];
+        $fileinfo = pathinfo($_FILES['image']['name']);//getting file info from the request
+        $extension = $fileinfo['extension']; //getting the file extension
+        $file_url = $upload_url . getFileName() . '.' . $extension; //file url to store in the database
+        $file_path = $upload_path . getFileName() . '.'. $extension; //file path to upload in the server
+        $img_name = getFileName() . '.'. $extension; //file name to store in the database
+ 
+        try{
+            move_uploaded_file($_FILES['image']['tmp_name'],$file_path); //saving the file to the uploads folder;
+           
+            //adding the path and name to database
+            // $sql = "INSERT INTO monan(tenmonan,nguyenlieu , congthuc,hinhanh,maloaimonan,manguoidung) ";
+            // $sql .= "VALUES ('{$tenmonan}', '{$nguyenlieu}', '{$congthuc}','{$file_url},'{$maloaimonan}','{$manguoidung}');";
+             $sql = "INSERT INTO nguoidung(manguoidung, email, tenhienthi,matkhau,maloainguoidung, hinhanh) VALUES (null, '$email','$tenhienthi','$matkhau','$maloainguoidung','$file_url')";
+    
+            if(mysqli_query($connection,$sql)){
+                //filling response array with values
+                $response['error'] = false;
+                $response['email'] = $email;
+                $response['tenhienthi'] = $tenhienthi;
+                $response['matkhau'] = $matkhau;
+                $response['hinhanh']=$file_url;
+                $response['maloainguoidung']=$maloainguoidung;
+            }
+            //if some error occurred
+        }catch(Exception $e){
+            $response['error']=true;
+            $response['message']=$e->getMessage();
+        }
+        //displaying the response
+        echo json_encode($response);
+ 
+        //closing the connection
+        mysqli_close($connection);
+    }else{
+        $response['error'] = true;
+        $response['message']='Please choose a file';
+    }
+}
+ 
+/*
+We are generating the file name
+so this method will return a file name for the image to be uploaded
+*/
+function getFileName(){
+    global $connection;
+     
+    $sql = "SELECT max(manguoidung) as manguoidung FROM nguoidung";
+    $result = mysqli_fetch_array(mysqli_query($connection, $sql));
+ 
+    if($result['manguoidung']== null)
+        return 1;
+    else
+        return ++$result['manguoidung'];
+     
+    mysqli_close($connection);
+}
+?>
--- app/src/main/java/com/example/appnauan/ui/dangky/DangKyFragment.java
+++ app/src/main/java/com/example/appnauan/ui/dangky/DangKyFragment.java
@@ -1,6 +1,9 @@
 package com.example.appnauan.ui.dangky;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -18,6 +21,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -25,9 +30,16 @@ import com.example.appnauan.MainActivity;
 import com.example.appnauan.R;
 import com.example.appnauan.ui.dangnhap.DangNhapFragment;
 
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
+
+import static android.app.Activity.RESULT_OK;
 
 public class DangKyFragment extends Fragment {
 
@@ -38,9 +50,12 @@ public class DangKyFragment extends Fragment {
     EditText edtTenHienThi;
     ImageView imgHinhAnh;
     public View view;
-    final int REQUEST_CODE_CAMERA=123;
+    private Uri filePath;
+    private int counter=0;
+    private Bitmap bitmap;
+    final int IMAGE_REQUEST_CODE=123;
     final int REQUEST_CODE_FOLDER=456;
-
+    private static final String URL_INSERT_USER = "http://10.80.255.137:8080/dbappnauan/insertUser.php";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,18 +64,14 @@ public class DangKyFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_dangky, container, false);
         view=root;
         AnhXa();
+        requestStoragePermission();
         imgHinhAnh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(Intent.ACTION_PICK);
+                Intent intent = new Intent();
                 intent.setType("image/*");
-                startActivityForResult(intent,REQUEST_CODE_FOLDER);
-//                ActivityCompat.requestPermissions(
-//                        getActivity(),
-//                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-//                        REQUEST_CODE_FOLDER
-//                );
-
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), IMAGE_REQUEST_CODE);
             }
         });
 
@@ -68,31 +79,51 @@ public class DangKyFragment extends Fragment {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String tenhienthi=edtTenHienThi.getText().toString();
-                String email=edtEmail.getText().toString();
-                String matkhau=edtPassWord.getText().toString();
+                String tenhienthi=edtTenHienThi.getText().toString().trim();
+                String email=edtEmail.getText().toString().trim();
+                String matkhau=edtPassWord.getText().toString().trim();
                 BitmapDrawable bitmapDrawable= (BitmapDrawable) imgHinhAnh.getDrawable();
                 Bitmap bitmap=bitmapDrawable.getBitmap();
                 ByteArrayOutputStream byteArray=new ByteArrayOutputStream();
-                String loaiuser="User";
                 bitmap.compress(Bitmap.CompressFormat.PNG,100,byteArray);
-                //chuyển về mảng byte[]
-                byte[] hinhAnh=byteArray.toByteArray();
-                Toast.makeText(getActivity(),"Đăng ký thành công",Toast.LENGTH_SHORT).show();
-                edtEmail.setText("");
-                edtPassWord.setText("");
-                //.setText("");
-                imgHinhAnh.setImageResource(R.drawable.noimage);
-                DangNhapFragment nextFrag= new DangNhapFragment();
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.nav_host_fragment, nextFrag, "findThisFragment")
-                        .addToBackStack(null)
-                        .commit();
+                Bitmap emptyBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+                if(tenhienthi.isEmpty() || email.isEmpty() || matkhau.isEmpty() || bitmap.sameAs(emptyBitmap))
+                {
+                    Toast.makeText(getActivity(),"Vui lòng nhập đầy đủ thông tin",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    ThemUser();
+                    edtEmail.setText("");
+                    edtPassWord.setText("");
+                    edtPassWord.setText("");
+                    imgHinhAnh.setImageResource(R.drawable.avatar);
+                    Toast.makeText(getActivity(),"Đăng ký thành công",Toast.LENGTH_SHORT).show();
+                    DangNhapFragment nextFrag= new DangNhapFragment();
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.nav_host_fragment, nextFrag, "findThisFragment")
+                            .addToBackStack(null)
+                            .commit();
+                }
+
             }
         });
                 return root;
 
     }
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(MainActivity.instance.getContentResolver(), filePath);
+                imgHinhAnh.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
     public void AnhXa()
     {
         btnSignUp=(Button) view.findViewById(R.id.btn_sign_up);
@@ -101,24 +132,80 @@ public class DangKyFragment extends Fragment {
         edtTenHienThi=(EditText)view.findViewById(R.id.et_tenhienthi);
         imgHinhAnh=(ImageView) view.findViewById(R.id.imageViewHinhUser);
     }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(REQUEST_CODE_CAMERA==requestCode  && data!=null)
-        {
-            Bitmap bitmap= (Bitmap) data.getExtras().get("data");
-            imgHinhAnh.setImageBitmap(bitmap);
+    public String getPath(Uri uri) {
+        Cursor cursor = MainActivity.instance.getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = MainActivity.instance.getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+        return path;
+    }
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
         }
-        if(REQUEST_CODE_FOLDER==requestCode  &&data!=null)
-        {
-            Uri uri=data.getData();
-            try {
-                InputStream inputStream=getActivity().getContentResolver().openInputStream(uri);
-                Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
-                imgHinhAnh.setImageBitmap(bitmap);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_FOLDER);
+    }
+
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if (requestCode == REQUEST_CODE_FOLDER) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Displaying a toast
+                Toast.makeText(getActivity(), "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(getActivity(), "Oops you just denied the permission", Toast.LENGTH_LONG).show();
             }
         }
-        super.onActivityResult(requestCode, resultCode, data);
     }
+    //Thêm user
+
+    public void ThemUser() {
+        //String caption = etCaption.getText().toString().trim();
+        String Email=edtEmail.getText().toString().trim();
+        String TenHienThi=edtTenHienThi.getText().toString().trim();
+        String MatKhau=edtPassWord.getText().toString().trim();
+        //getting the actual path of the image
+        String path = getPath(filePath);
+
+        //Uploading code
+        try {
+            String uploadId = UUID.randomUUID().toString();
+
+            //Creating a multi part request
+            new MultipartUploadRequest(getActivity(), uploadId, URL_INSERT_USER)
+                    .addFileToUpload(path, "image") //Adding file
+                    .addParameter("email", Email)
+                    .addParameter("tenhienthi",TenHienThi)
+                    .addParameter("matkhau",MatKhau)
+                    .addParameter("maloainguoidung","2")
+                    .setUtf8Charset()
+                    //Adding text parameter to the request
+                    .setNotificationConfig(new UploadNotificationConfig())
+                    .setMaxRetries(2)
+                    .startUpload(); //Starting the upload
+        } catch (Exception exc) {
+            Toast.makeText(getActivity(), exc.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
